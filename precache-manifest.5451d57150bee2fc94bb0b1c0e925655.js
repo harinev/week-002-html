1aa2c167c86b9be790a314c6af7a3b01bb62b9f0
harine <harinev@outlook.com>
2019-12-08 23:36:09 +0000

    Updates

---
+++ precache-manifest.5451d57150bee2fc94bb0b1c0e925655.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee07995696462526908f2456dba3d23",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "811aa84230818273f2f2",
+    "url": "/todo-list-app/static/css/main.045e7c20.chunk.css"
+  },
+  {
+    "revision": "b91a108a3a16b924d4fb",
+    "url": "/todo-list-app/static/js/2.545fce84.chunk.js"
+  },
+  {
+    "revision": "811aa84230818273f2f2",
+    "url": "/todo-list-app/static/js/main.5b026040.chunk.js"
+  },
+  {
+    "revision": "68cd38656ee03f67f234",
+    "url": "/todo-list-app/static/js/runtime-main.0d2e15b2.js"
+  }
+]);
