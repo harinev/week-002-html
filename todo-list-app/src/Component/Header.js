7aeb593fb25063b1c82b2f36d2b8fc51235d8155
harine <harinev@outlook.com>
2019-12-09 18:15:42 +0000

    remove todo-list-app react project

--- todo-list-app/src/Component/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-function Header() {
-    return (
-        <h1><center>Harine's Todo Task Listing Page</center></h1>
-    )
-}
-export default Header;
--- todo-list-app/src/Component/Input.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Input() {
-    return (
-        
-        <h3>Want to add a new Task?</h3>
-        
-    )
-}
-export default Input;
